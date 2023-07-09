e81f5dc27a3515fc8a53711bb082177b5515eafa
tessa-tum <125835305+tessa-tum@users.noreply.github.com>
2023-07-09 22:25:22 +0200

    integrate final api

--- src/api.js
+++ src/api.js
@@ -1,18 +1,112 @@
-import mockData from './mock-data';
+import mockData from "./mock-data";
+
 /**
+ *
  * @param {*} events:
+ * The following function should be in the “api.js” file.
  * This function takes an events array, then uses map to create a new array with only locations.
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
+
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
   const locations = [...new Set(extractedLocations)];
   return locations;
 };
-/**
- * This function will fetch the list of all events
- */
+
+// get all events
 export const getEvents = async () => {
-  return mockData;
+  // when using localhost, return mock data
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
+
+  // return real API data, when token is found
+  // make GET request to Google API via get-events-endpoint
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url =
+      "https://j7brorfc5myoqxco3bpk3bsiyq0lszsl.lambda-url.eu-central-1.on.aws/api/get-events/" +
+      "/" +
+      token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      return result.events;
+    } else return null;
+  }
+};
+
+// get access token
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+
+  // if no token found in local storage, redirect to Google Auth screen
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://n264hn5574w2xq7nyt5en4wdsa0mlozf.lambda-url.eu-central-1.on.aws/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+// if token found in local storage, check validity (if not valid, redirect to Google Auth screen)
+const checkToken = async (accessToken) => {
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
+};
+
+// remove unnecessary URL query parameters
+const removeQuery = () => {
+  let newurl;
+  if (window.history.pushState && window.location.pathname) {
+    newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+// if token does not exist, or is invalid, get a new one
+// take auth code and encode it using encodeURIComponent, then use the encoded code to get the token.
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+
+    const response = await fetch(
+      "https://rescmuarp6dy752of75f4u2f7e0igyca.lambda-url.eu-central-1.on.aws/api/token" +
+        "/" +
+        encodeCode
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    error.json();
+  }
 };
