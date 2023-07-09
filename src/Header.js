5bfce56aaa7f103a899dcc2086a4e91499990221
tessa-tum <125835305+tessa-tum@users.noreply.github.com>
2023-07-09 22:25:43 +0200

    add styles

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import devtrecLogo from "./assets/devtrec_logo_cropped.png";
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <img className="devtrec-logo" src={devtrecLogo} alt="Devtrec App Logo" />
+      <p className="logo-subline">Find your favorite dev events</p>
+    </header>
+  );
+};
+
+export default Header;
