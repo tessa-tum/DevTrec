6c97579fef23144aa75ff84ee5a1f5668eb0f3d8
tessa-tum <125835305+tessa-tum@users.noreply.github.com>
2023-07-09 22:22:39 +0200

    integration-tests-implementation (feat 1, scenario 1 and 3 + feat 3, scenario 2)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
+import Header from "./Header";
+import { useEffect, useState } from "react";
+import { extractLocations, getEvents } from "./api";
+
 import "./App.css";
 
 const App = () => {
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [events, setEvents] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity, currentNOE]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents =
+      currentCity === "See all cities"
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
+
   return (
     <div className="App">
-      <CitySearch />
-      <EventList />
-      <NumberOfEvents />
+      <Header />
+      <div className="searches-container">
+        <CitySearch
+          allLocations={allLocations}
+          setCurrentCity={setCurrentCity}
+        />
+        <NumberOfEvents setCurrentNOE={setCurrentNOE} />
+      </div>
+      <div className="grid">
+        <EventList events={events} />
+      </div>
     </div>
   );
 };
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -2,29 +2,29 @@ import { render, screen } from "@testing-library/react";
 import NumberOfEvents from "../components/NumberOfEvents";
 import userEvent from "@testing-library/user-event";
 
-describe("<NumberOfEvents /> component", () => {
+// unit testing
 
+describe("<NumberOfEvents /> component", () => {
   test("render text input", () => {
-    render(<NumberOfEvents />);
-    const numberInput = screen.getByTestId("number-input");
-    expect(numberInput).toBeInTheDocument();
-    expect(numberInput).toHaveClass("events-number");
+    render(<NumberOfEvents setCurrentNOE={() => {}} />);
+    const number = screen.getByTestId("number-of-events-input");
+    expect(number).toBeInTheDocument();
   });
 
   test("default value of the input field is 32", () => {
-    render(<NumberOfEvents />);
-    const numberInput = screen.getByTestId("number-input");
-    expect(numberInput).toBeInTheDocument();
-    expect(numberInput).toHaveValue(32);
+    render(<NumberOfEvents setCurrentNOE={() => {}} />);
+    const number = screen.getByTestId("number-of-events-input");
+    expect(number).toBeInTheDocument();
+    expect(number).toHaveValue("32");
   });
 
   test("value of input field changes when user types in it", async () => {
     const user = userEvent.setup();
-    render(<NumberOfEvents />);
-    const numberInput = screen.getByTestId("number-input");
-    expect(numberInput).toBeInTheDocument();
-    expect(numberInput).toHaveValue(32);
-    await user.type(numberInput, "{backspace}{backspace}14");
-    expect(numberInput).toHaveValue(14);
+    render(<NumberOfEvents setCurrentNOE={() => {}} />);
+    const number = screen.getByTestId("number-of-events-input");
+    expect(number).toBeInTheDocument();
+    expect(number).toHaveValue("32");
+    await user.type(number, "{backspace}{backspace}14");
+    expect(number).toHaveValue("14");
   });
 });
