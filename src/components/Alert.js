b48a47c8545dd499df6e1bf512284d3b7724316f
tessa-tum <125835305+tessa-tum@users.noreply.github.com>
2023-07-18 03:54:59 +0200

    implement warning alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -39,8 +39,8 @@ class InfoAlert extends Alert {
 class WarningAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = "orange";
-    this.bgColor = "rgb(255,231, 196)"; // light orange
+    this.color = "rgb(255,94,37";
+    this.bgColor = "rgb(255,219, 155)"; 
   }
 }
 
