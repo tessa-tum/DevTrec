b44d0a055abd99e95b54497e701274f277164335
tessa-tum <125835305+tessa-tum@users.noreply.github.com>
2023-07-18 19:21:12 +0200

    fix CityEventsChart + tests setup

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import {
   ScatterChart,
   Scatter,
-  XAxis, YAxis,
+  XAxis,
+  YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer
-} from 'recharts';
+  ResponsiveContainer,
+} from "recharts";
 
 const CityEventsChart = ({ allLocations, events }) => {
   const [data, setData] = useState([]);
@@ -17,10 +18,12 @@ const CityEventsChart = ({ allLocations, events }) => {
 
   const getData = () => {
     const data = allLocations.map((location) => {
-      const count = events.filter((event) => event.location === location).length
-      const city = location.split(', ')[0]
+      const count = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split((/, | - /))[0]
       return { city, count };
-    })
+    });
     return data;
   };
 
@@ -28,20 +31,32 @@ const CityEventsChart = ({ allLocations, events }) => {
     <ResponsiveContainer width="99%" height={400}>
       <ScatterChart
         margin={{
-          top: 20,
-          right: 10,
-          bottom: 20,
-          left: 0,
+          top: 60,
+          right: 20,
+          bottom: 60,
+          left: -30,
         }}
       >
         <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" />
-        <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
-        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <XAxis
+          type="category"
+          dataKey="city"
+          name="City"
+          angle={60}
+          interval={0}
+          tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
+        <YAxis
+          type="number"
+          dataKey="count"
+          name="Number of events"
+          allowDecimals={false}
+        />
+        <Tooltip cursor={{ strokeDasharray: "3 3" }} />
         <Scatter name="A city" data={data} fill="#fa4659" />
       </ScatterChart>
     </ResponsiveContainer>
   );
-}
+};
 
 export default CityEventsChart;
