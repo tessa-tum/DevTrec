278c14206f59fa36d1784307a6a51fb3e45e6cf0
tessa-tum <125835305+tessa-tum@users.noreply.github.com>
2023-07-18 02:35:26 +0200

    add nprogress custom

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert } from "./components/Alert";
 import "./App.css";
+import './nprogress.css';
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
